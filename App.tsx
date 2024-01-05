704d8f7d3f957c1476edf9c01a544fc65c242433
uuganbat <uuganbatdev@uuganbats-MacBook-Pro.local>
2024-01-05 15:33:43 +0800

    react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, TouchableOpacity, Button} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <View className='w-44 h-44 bg-red-100'>
-        <Text>hello</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen(): React.JSX.Element {
+  const navigation = useNavigation();
+
+  return (
+    <View className="bg-red-100">
+      <Text>Home Screen</Text>
+      <Button
+        title="Login"
+        onPress={() => {
+          navigation.navigate('Login');
+        }}
+      />
+    </View>
+  );
+}
+
+function LoginScreen(): React.JSX.Element {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Login Screen</Text>
+    </View>
   );
 }
 
--- app.d.ts
+++ app.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="nativewind/types" />
+type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+};
+
+namespace ReactNavigation {
+  interface RootParamList extends RootStackParamList {}
+}
